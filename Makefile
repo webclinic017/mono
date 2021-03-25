5df562f46ec8216faf648d8f77db4709a20bdb59
Jeremy Muhia <jmuhia80@gmail.com>
2021-03-24 22:44:26 -0400

    [Backend] remove docker compose artifacts (#32)

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 SOURCE := $(shell git rev-parse --show-toplevel)
 
 include $(SOURCE)/scripts/make/build.mk
-# include $(SOURCE)/scripts/make/test.mk
 
--- scripts/make/build.mk
+++ scripts/make/build.mk
@@ -1,28 +1,7 @@
-.PHONY: build clean coldstart deepclean logs proto start status stop
-
-build: # Build docker image
-	docker-compose build
-
-clean:stop # Stop docker containers, clean data and workspace
-	docker-compose down -v --remove-orphans
-
-coldstart: # Build and start docker images
-	docker-compose up --detach --build
+.PHONY: deepclean
 
 deepclean: # Deep clean local docker
 	docker container prune -f
 	docker image prune -a -f
 	docker volume prune -f
-
-logs: # Get logs of containers
-	docker-compose logs -f
-
-start: # Start docker containers
-	docker-compose up -d
-
-status: # Get status of containers
-	docker-compose ps
-
-stop: # Stop docker containers
-	docker-compose stop
 
