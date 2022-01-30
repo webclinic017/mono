e0bc78b5d5505256544f5baa30e9230b79a6c0a1
Jeremy Muhia <jmuhia80@gmail.com>
2022-01-30 16:58:36 -0500

    [Python][Tools] clean up notebooks (#79)

--- tools/bash/start_local_runtime.sh
+++ tools/bash/start_local_runtime.sh
@@ -2,7 +2,7 @@
 
 # https://research.google.com/colaboratory/local-runtimes.html
 
-bazel run //python/notebooks:start_local_runtime -- \
+bazel run //python:start_local_runtime -- \
   --NotebookApp.allow_origin='https://colab.research.google.com' \
   --port=8888 \
   --NotebookApp.port_retires=0
